cd4af9a98fe38f79766e3578b68ef0eb32c9f84b
Divahar 😎 <codewithdivahar@gmail.com>
2025-08-30 19:50:18 -0400

    Update string concat

--- HelloWorld/src/StringConcat.java
+++ HelloWorld/src/StringConcat.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class StringConcat {
     static void main() {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first name: ");
         String firstName = scanner.next();
+        System.out.print("Enter last name: ");
         String lastName = scanner.next();
         System.out.println(conc(firstName,lastName));
     }
 
     public static String conc(String fname, String lname){
-        return fname.concat(lname);
+        return fname + " " + lname;
     }
 }
